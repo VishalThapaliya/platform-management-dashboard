ba7291f1db445fb309ee25051669238f482d26f5
VishalThapaliya <vishal.thapaliya@gmail.com>
2021-10-10 17:41:02 +0200

    Implement left navigation and topbar

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Add hoverd class to the selected list items
+let list = document.querySelectorAll('.navigation li');
+
+function activeLink() {
+   list.forEach((item) => {
+    item.classList.remove('hovered');
+    this.classList.add('hovered');
+   });
+}
+
+list.forEach((item) => {
+  item.addEventListener('mouseover', activeLink);
+});
